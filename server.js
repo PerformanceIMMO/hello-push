c98e270504d2909490c8eb147fda55f020b471cc
Nicolas Bétheuil <wadouk@gmail.com>
2019-08-30 16:46:06 +0200

    read process env or start on 8080

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json())
 // This serves static files from the specified directory
 app.use(express.static(__dirname));
 
-const server = app.listen(8081, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
 
   const host = server.address().address;
   const port = server.address().port;
